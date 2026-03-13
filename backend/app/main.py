c1a6299a8248fe4ed80e9abc0951f811c8481576
mason-liuuu <masonliu116@gmail.com>
2026-03-13 11:59:28 +0900

    minor adjust

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,7 @@
 from app.routers import auth, payments, restaurants
 from app.routers.orders import router as orders_router
 from app.routers.search_router import router as search_router
-from fastapi import FastAPI, Request
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
 
 app = FastAPI(title="GitSOS Backend")
 
@@ -16,21 +14,6 @@ def read_root():
 @app.get("/health")
 def health():
     return {"status": "ok"}
-
-@app.exception_handler(RequestValidationError)
-async def request_validation_exception_handler(
-    request: Request,
-    exc: RequestValidationError,
-):
-    del request
-
-    return JSONResponse(
-        status_code=422,
-        content={
-            "message": "Invalid request parameters.",
-            "details": exc.errors(),
-        },
-    )
 
 # Include routers
 app.include_router(orders_router)
